992c6c462b0c1f51df7155f0a69908451de64e4e
plusmate <psejin1478@gmail.com>
2022-01-22 11:40:49 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import { useState } from "react";
 import DisplayedGoods from "./states/GoodsState.js";
 import { Link, Route, Switch } from "react-router-dom";
 import Detail from "./Components/Detail";
+import axios from "axios";
 
 function App() {
   let [shoes, changingShoes] = useState(DisplayedGoods);
+  let [count, changingCount] = useState(2);
+  let [countingGoods, changingCountingGoods] = useState(2);
+  let [activeBtn, changingActiveBtn] = useState(true);
+  let [loadingImage, changingLoadingImage] = useState(false);
+  let [stock, changingStock] = useState([10, 3, 51, 51, 600, 880, 500, 400, 139])
 
   return (
     <div className="App">
@@ -18,11 +24,11 @@ function App() {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link>
-                <Link to="/">Home</Link>
+              <Nav.Link as={Link} to="/">
+                Home
               </Nav.Link>
-              <Nav.Link>
-                <Link to="/detail">Detail</Link>
+              <Nav.Link as={Link} to="/detail">
+                Detail
               </Nav.Link>
               <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
@@ -59,10 +65,66 @@ function App() {
           </Link>
 
           <GoodsData shoes={shoes} />
+          {activeBtn === true ? (
+            <>
+              <button
+                className="btn btn-primary"
+                onClick={() => {
+                  changingCount(count + 1);
+                  changingCountingGoods(countingGoods + 3);
+                  //fetch('링크').then();
+                  /*로딩중이미지 생성 */
+                  changingLoadingImage(true);
+
+                  axios.post('서버URL', {id: 'SejinPark', pw: 1478}).then();
+
+                  axios
+                    .get(
+                      "https://codingapple1.github.io/shop/data" +
+                        count +
+                        ".json"
+                    )
+                    .then(result => {
+                      //데이터를 가져오는데 성공했을떄의 결과(데이터는 파라미터로 저장됨)
+                      /*로딩중이미지 삭제 */
+                      changingLoadingImage(false);
+                      changingShoes([...shoes, ...result.data]); //changingState의 배열에 다른 배열을 합침
+                      /*
+                      let shoesArray = [...shoes];
+                      shoesArray.push(...result.data)
+                      changingShoes(shoesArray)
+                      */
+                      {
+                        shoes.length - 1 === countingGoods &&
+                        shoes.length - 1 > 2
+                          ? changingActiveBtn(false)
+                          : changingActiveBtn(true);
+                      }
+                    })
+                    .catch(() => {
+                      /*로딩중이미지 삭제 */
+                      console.log("실패");
+                      let activeBtnArray = [...activeBtn];
+                      activeBtnArray = false;
+                      changingActiveBtn(false);
+                    }); //해당 링크에 대한 데이터를 가져옴
+                }}
+              >
+                더보기
+              </button>
+              {loadingImage === true ? (
+                <div className="loading-image-cover">
+                  <div className="loading-left"></div>
+                  <div className="loading-font">Loading...</div>
+                  <div className="loading-right"></div>
+                </div>
+              ) : null}
+            </>
+          ) : null}
         </Route>
 
         <Route path="/detail/:shoesNumbers">
-          <Detail shoes={shoes} />
+          <Detail shoes={shoes} stock={stock} changingStock={changingStock}/>
         </Route>
 
         <Route path=":id">
@@ -75,24 +137,28 @@ function App() {
 
 function GoodsData(props) {
   return (
-    <div className="row">
-      {props.shoes.map((shoesData) => {
-        return (
-          <div className="col-md-4">
-            <Link to={"/detail/" + (shoesData.id)}>
-              <img
-                src={
-                  "https://codingapple1.github.io/shop/shoes" + (shoesData.id + 1) + ".jpg"
-                }
-                width="100%"
-              />
-              <h4>{shoesData.title}</h4>
-              <p>{shoesData.content}</p>
-              <h3>{shoesData.price}₩</h3>
-            </Link>
-          </div>
-        );
-      })}
+    <div className="container">
+      <div className="row">
+        {props.shoes.map(shoesData => {
+          return (
+            <div className="col-md-4">
+              <Link to={"/detail/" + shoesData.id}>
+                <img
+                  src={
+                    "https://codingapple1.github.io/shop/shoes" +
+                    (shoesData.id + 1) +
+                    ".jpg"
+                  }
+                  width="100%"
+                />
+                <h4>{shoesData.title}</h4>
+                <p>{shoesData.content}</p>
+                <h3>{shoesData.price}₩</h3>
+              </Link>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
--- src/Components/Detail.js
+++ src/Components/Detail.js
@@ -1,25 +1,108 @@
-import React from "react";
+import { Axios } from "axios";
+import React, { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+import "../Detail.scss";
+
+/*
+useEffect 옛날버전
+class Detail2 extends React.Component {
+  componentDidMount() {} //해당 컴포넌트가 렌더링 될 때 실행하는 함수
+
+  componentWillUnmount() {} //컴포넌트를 끝낼 때 실행하는 함수
+}
+*/
+
+let DivBox = styled.div`
+  padding: 20px;
+`;
+
+let TitleCss = styled.h4`
+  font-size: 25px;
+  color: ${props => props.ColorCss};
+`;
 
 function Detail(props) {
-
+  let [timeout, changingTimeout] = useState(true);
+  let [disableAlert, changingDisableAlert] = useState(false);
+  let [inputValue, changingInputValue] = useState("");
   let { shoesNumbers } = useParams(); //사용자가 입력한 파라미터가 id라는 변수로 들어감
   let history = useHistory();
 
+  function changingStockFunction() {
+    let stockArray = [...props.stock];
+    stockArray[shoesNumbers] = stockArray[shoesNumbers] - 1;
+    props.changingStock(stockArray);
+    {
+      stockArray[shoesNumbers] <= 0
+        ? changingDisableAlert(true)
+        : changingDisableAlert(false);
+    }
+  }
+
+  useEffect(() => {
+    // axios.get();
+
+    //2초후에 alert창이 나타났다 사라졌다 하는 코드
+    let timer = setInterval(() => {
+      changingTimeout(!timeout);
+    }, 2000);
+    return () => {
+      clearInterval(timer);
+    }; //창을 나가면 timer해제
+  }, [timeout]);
+
+  // useEffect(()=>{
+  //   let timer = setTimeout(()=>{ alert변경(false) }, 2000);
+  // })
+
   return (
     <div className="container">
+      <DivBox>
+        <TitleCss className="red">상세페이지</TitleCss>
+        <TitleCss ColorCss={"blue"}>상세페이지</TitleCss>
+      </DivBox>
+
+      <input
+        className="goods-search"
+        onChange={e => {
+          changingInputValue(e.target.value);
+        }}
+      />
+
       <div className="row">
         <div className="col-md-6">
           <img
-            src={"https://codingapple1.github.io/shop/shoes" + (props.shoes[shoesNumbers].id+1)  + ".jpg"}
+            src={
+              "https://codingapple1.github.io/shop/shoes" +
+              (props.shoes[shoesNumbers].id + 1) +
+              ".jpg"
+            }
             width="100%"
           />
         </div>
         <div className="col-md-6 mt-4">
           <h4 className="pt-5">{props.shoes[shoesNumbers].title}</h4>
           <p>{props.shoes[shoesNumbers].content}</p>
-          <h3>{props.shoes[shoesNumbers].price}￦</h3>
-          <button className="btn btn-danger">주문하기</button>
+          <h3>{props.shoes[shoesNumbers].price}₩</h3>
+          <StockFunction
+            stock={props.stock}
+            shoes={props.shoes}
+            shoesNumbers={shoesNumbers}
+            changingStockFunction={changingStockFunction}
+          ></StockFunction>
+          {
+            (disableAlert === true ? null : (
+              <button
+                className="btn btn-danger"
+                onClick={() => {
+                  changingStockFunction();
+                }}
+              >
+                주문하기
+              </button>
+            ))
+          }
           &nbsp;
           <button
             className="btn btn-danger"
@@ -32,8 +115,28 @@ function Detail(props) {
           </button>
         </div>
       </div>
+
+      {props.stock[shoesNumbers] <= 50 ? (
+        timeout === true && disableAlert === false ? (
+          <div className="my-alert my-alert2 stock-alert">
+            <p className="low-in-stock">재고가 얼마 남지 않았습니다</p>
+          </div>
+        ) : null
+      ) : null}
+
+      {props.stock[shoesNumbers] === 0 ? (
+        disableAlert === true ? (
+          <div className="my-alert my-alert2 stock-alert">
+            <p className="low-in-stock">품절되었습니다.</p>
+          </div>
+        ) : null
+      ) : null}
     </div>
   );
+}
+
+function StockFunction(props) {
+  return <p>재고 : {props.stock[props.shoesNumbers]}</p>;
 }
 
 export default Detail;
